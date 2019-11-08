2da19ecf3a3e3bbad27002abb39a60931905a970
QuocVinh <quocvinhnguyen97@gmail.com>
2019-11-08 16:38:50 +0700

    sdas
    ádas

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoACS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point> points = TspFileReader.ReadTspFile(@"C:\Users\QuocVinh\Downloads\Compressed\asc\asc\test\demo.tsp");
+
+            DoThi dothi = new DoThi(points, true);
+            GreedyAlgorithm greedyAlgorithm = new GreedyAlgorithm(dothi);
+            double greedyShortestTourDistance = greedyAlgorithm.Run();
+
+            Parameters parameters = new Parameters()
+            {
+                T0 = (1.0 / (dothi.Dimensions * greedyShortestTourDistance))
+            };
+            parameters.Show();
+
+            Solver solver = new Solver(parameters, dothi);
+            List<double> results = solver.RunACS();
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ACS
-ACS
+# acs
+
